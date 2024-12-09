e13bbc14f724b023c9d1af533301e44c3b4f1ab6
Abdullah Malik <81826433+Abdullah-Malik-39@users.noreply.github.com>
2024-12-09 14:43:22 +0500

    Initial Commit

--- index.java
+++ index.java
@@ -1,6 +1,6 @@
 public class index {
     public static void main(String[] args) {
-        int number = 64;
+        int number = 32;
         int square = number * number;
         System.out.println("The square of " + number + " is: " + square);
     }
